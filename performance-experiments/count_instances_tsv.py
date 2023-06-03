f3d22757c1de50b3398a68e99338a2e66f5d1a96
seyedahbr <a.beghaei@gmail.com>
2023-06-03 14:35:18 +0100

    Added returning the list of genes + more accurate query

--- performance-experiments/count_instances_tsv.py
+++ performance-experiments/count_instances_tsv.py
@@ -3,19 +3,20 @@ import pandas as pd
 
 def count_instances(tsv_file):
     df = pd.read_csv(tsv_file, sep='\t')
-    genes = df.loc[(df['node2'].eq('Q7187')) & (df['id'].str.contains('-P31-'))]['node1'].values.tolist()
-    proteins = df.loc[(df['node2'].eq('Q8054')) & (df['id'].str.contains('-P31-'))]['node1'].values.tolist()
-    chemicals = df.loc[(df['node2'].eq('Q11173')) & (df['id'].str.contains('-P31-'))]['node1'].values.tolist()
-    diseases = df.loc[(df['node2'].eq('Q12136')) & (df['id'].str.contains('-P31-'))]['node1'].values.tolist()
-    operon_match = len(df.loc[(df['node2'].eq('Q139677')) & (df['id'].str.contains('-P31-'))])
-    acid_match = len(df.loc[(df['node2'].eq('Q11158')) & (df['id'].str.contains('-P31-'))])
+    genes = df.loc[(df['node2'].eq('Q7187')) & (df['id'].str.contains('-P31-Q'))]['node1'].values.tolist()
+    proteins = df.loc[(df['node2'].eq('Q8054')) & (df['id'].str.contains('-P31-Q'))]['node1'].values.tolist()
+    chemicals = df.loc[(df['node2'].eq('Q11173')) & (df['id'].str.contains('-P31-Q'))]['node1'].values.tolist()
+    diseases = df.loc[(df['node2'].eq('Q12136')) & (df['id'].str.contains('-P31-Q'))]['node1'].values.tolist()
+    operon_match = len(df.loc[(df['node2'].eq('Q139677')) & (df['id'].str.contains('-P31-Q'))])
+    acid_match = len(df.loc[(df['node2'].eq('Q11158')) & (df['id'].str.contains('-P31-Q'))])
 
     gene_statements = len(df.loc[df['node1'].isin(genes)])
     protein_statements = len(df.loc[df['node1'].isin(proteins)])
     chemical_statements = len(df.loc[df['node1'].isin(chemicals)])
     disease_statements = len(df.loc[df['node1'].isin(diseases)])
     
-    
+    with open('genes_output.txt', 'w') as f:
+        [f.write(f'{line}\n') for line in genes]
     print('Genes: ', len(genes))
     print('Proteins: ', len(proteins))
     print('Chemical Compounds: ', len(chemicals))
